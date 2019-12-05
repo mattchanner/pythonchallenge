93658f334bd2bcd5c7db3bb3e454ff5ce5264f9a
Matt Channer <matt.channer@referencepoint.co.uk>
2019-12-05 09:20:10 +0000

    challenge 7

---
+++ 7/main.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+img = Image.open("oxygen.png")
+row = [img.getpixel((x, int(img.height / 2))) for x in range(img.width)]
+
+# colours are in blocks 7 pixels wide, so use this as the stride
+chars = [chr(r) for r, g, b, a in row[::7] if r == g == b]
+
+print(''.join(chars))
+
+# smart guy, you made it. the next level is [105, 110, 116, 101, 103, 114, 105, 116, 121]
+hint = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+print(''.join(chr(x) for x in hint))
+
+# integrity
